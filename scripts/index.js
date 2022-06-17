c87a54ea3166c08db3b6f1bfd553d5e0fa54c1a0
Alex Gus <radiogusarov@gmail.com>
2022-06-17 16:34:46 +0300

    Исправить ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,42 @@
-const editButton = document.querySelector('.button_action_edit');
-
+const editButton = document.querySelector('.profile__button-edit');
 const popup = document.querySelector('.popup');
-
 const submitButton = popup.querySelector('.popup__submit');
-
 const closeButton = document.querySelector('.popup__button-close');
-
 const popupForm = document.querySelector('.popup__form');
+let nameInput = popupForm.querySelector('.popup__input-text_name');
+let jobInput = popupForm.querySelector('.popup__input-text_job');
+let nameProfile = document.querySelector('.profile__heading');
+let jobProfile = document.querySelector('.profile__subheading'); 
 
-popupForm.addEventListener('submit', submitHandler);
+function openPopup () {
+  popup.classList.add('popup_opened');
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+}
 
 function closePopup () {
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', openPopup);
-
-function openPopup () {
-  popup.classList.add('popup_opened');
-}
-
-closeButton.addEventListener('click', closePopup);
-
 function submitHandler (event) {
-  event.preventDefault();
-  let nameInput = popupForm.querySelector('.popup__name');
-  let jobInput = popupForm.querySelector('.popup__job');
-  let nameProfile = document.querySelector('.profile__heading');
-  let jobProfile = document.querySelector('.profile__subheading'); 
+  event.preventDefault();  
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
   closePopup();  
 } 
+
+editButton.addEventListener('click', openPopup);
+
+closeButton.addEventListener('click', closePopup);
+
+popupForm.addEventListener('submit', submitHandler);
+
+
+
+
+
+
+
+
+
 
